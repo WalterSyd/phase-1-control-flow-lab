7b0404bf408db9c645092af0c854af8391b9690e
Waltersyd <sydney.onyango@student.moringaschool.com>
2024-06-28 16:49:04 +0300

    Test index.js

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+
+function scuberGreetingForFeet(x){
+  if(x <= 400){
+    return "This one is on me!";
+  }else if(x > 400 && x <= 2000){
+    return "That will be twenty bucks.";
+  }else if( x >2000 && x <= 2500){
+    return "I will gladly take your thirty bucks.";
+  }else {
+    return "No can do."
+    }
+  }
+    
+function ternaryCheckCity(myCity){
+  return myCity === "NYC" ? "Ok, sounds good." : "No go."
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(Tip){
+  switch (Tip){
+  case "generous":
+    return "Thank you so much.";
+  case "not as generous":
+    return "Thank you.";
+  default:
+      return "Bye."
+  }
 }
